1774139f668ed59dbe541d364995052804f1b649
Shrirang <shrirang.sapate95@gmail.com>
2023-12-10 21:21:35 +0530

    test3

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-// import '@fontsource/koho/200-italic.css';
-// import '@fontsource/koho/300-italic.css';
-// import '@fontsource/koho/400-italic.css';
-// import '@fontsource/koho/500-italic.css';
-// import '@fontsource/koho/600-italic.css';
-// import '@fontsource/koho/700-italic.css';
 import '@fontsource/koho/200.css';
 import '@fontsource/koho/300.css';
 import '@fontsource/koho/400.css';
@@ -16,19 +10,15 @@ import '@fontsource/koho/600.css';
 import '@fontsource/koho/700.css';
 // Supports weights 300-800
 import '@fontsource-variable/open-sans';
-
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 import 'boxicons/css/boxicons.min.css';
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
 import { library } from '@fortawesome/fontawesome-svg-core';
-
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
-// import { faSearch } from '@fortawesome/free-regular-svg-icons';
+
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
 
