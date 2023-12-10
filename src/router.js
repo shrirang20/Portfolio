d3d58bfbeef367ff465c65f8e99e65b2a2d565af
Shrirang <shrirang.sapate95@gmail.com>
2023-12-10 21:27:33 +0530

    comment answer3-8

--- src/router.js
+++ src/router.js
@@ -5,11 +5,11 @@ import StatsPage from './components/Stats.vue'
 import AnswerPageOne from './components/AnswerPageOne.vue'
 import AnswerPageTwo from './components/AnswerPageTwo.vue'
 // import AnswerPageThree from './components/AnswerPageThree.vue'
-import AnswerPageFour from './components/AnswerPageFour.vue'
-import AnswerPageFive from './components/AnswerPageFive.vue'
-import AnswerPageSix from './components/AnswerPageSix.vue'
-import AnswerPageSeven from './components/AnswerPageSeven.vue'
-import AnswerPageEight from './components/AnswerPageEight.vue'
+// import AnswerPageFour from './components/AnswerPageFour.vue'
+// import AnswerPageFive from './components/AnswerPageFive.vue'
+// import AnswerPageSix from './components/AnswerPageSix.vue'
+// import AnswerPageSeven from './components/AnswerPageSeven.vue'
+// import AnswerPageEight from './components/AnswerPageEight.vue'
 
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -45,31 +45,31 @@ const routes = [
     //     component:AnswerPageThree,
     //     path: '/3'
     // },
-    {
-        name:'Answer4',
-        component:AnswerPageFour,
-        path: '/4'
-    },
-    {
-        name:'Answer5',
-        component:AnswerPageFive,
-        path: '/5'
-    },
-    {
-        name:'Answer6',
-        component:AnswerPageSix,
-        path: '/6'
-    },
-    {
-        name:'Answer7',
-        component:AnswerPageSeven,
-        path: '/7'
-    },
-    {
-        name:'Answer8',
-        component:AnswerPageEight,
-        path: '/8'
-    }
+    // {
+    //     name:'Answer4',
+    //     component:AnswerPageFour,
+    //     path: '/4'
+    // },
+    // {
+    //     name:'Answer5',
+    //     component:AnswerPageFive,
+    //     path: '/5'
+    // },
+    // {
+    //     name:'Answer6',
+    //     component:AnswerPageSix,
+    //     path: '/6'
+    // },
+    // {
+    //     name:'Answer7',
+    //     component:AnswerPageSeven,
+    //     path: '/7'
+    // },
+    // {
+    //     name:'Answer8',
+    //     component:AnswerPageEight,
+    //     path: '/8'
+    // }
 ]
 
 const router = createRouter({
